c36061a63d4caa968c82ac75d0198d3a8de5ddbd
DaneLloyd11 <lloydaying123@gmail.com>
2026-05-01 15:24:43 +0800

    feat:
    Added ui for Admin login, Reset password for admin login, and student sit-in login.
    
    Added hide and show password.
    
    Added "Remember me" function. (Note: feel nako need pa e fix and remember me.)

--- Helpers/Converters.cs
+++ Helpers/Converters.cs
@@ -56,4 +56,17 @@ namespace LaboratorySitInSystem.Helpers
             throw new NotSupportedException();
         }
     }
+
+    public class StringToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !string.IsNullOrWhiteSpace(value as string) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
 }
