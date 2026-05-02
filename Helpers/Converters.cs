2f4e42310738d26acc7fa30b812bf6152a8f09d2
Elihu France Caalim <159905595+Paris1229@users.noreply.github.com>
2026-05-02 18:43:38 +0800

    fix: show data to the sitin interface schedule block (#38)

--- Helpers/Converters.cs
+++ Helpers/Converters.cs
@@ -138,4 +138,21 @@ namespace LaboratorySitInSystem.Helpers
             throw new NotSupportedException();
         }
     }
+
+    public class TimeSpanToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is TimeSpan timeSpan)
+            {
+                return timeSpan.ToString(@"hh\:mm");
+            }
+            return value?.ToString() ?? string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
 }
