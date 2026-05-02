a8de5d5f24cae24a9230e2421d1b8e5282fa1af4
Justinnnn15 <justinaldevera@gmail.com>
2026-05-02 10:48:44 +0800

    Implement About page with carousel, member photos, names and mottos

--- Helpers/Converters.cs
+++ Helpers/Converters.cs
@@ -2,6 +2,7 @@ using System;
 using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
+using System.Windows.Media;
 
 namespace LaboratorySitInSystem.Helpers
 {
@@ -62,6 +63,41 @@ namespace LaboratorySitInSystem.Helpers
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             return !string.IsNullOrWhiteSpace(value as string) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+
+    public class DotColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isSelected = value is true;
+            if (isSelected)
+            {
+                return Application.Current.Resources["PrimaryBrush"] as SolidColorBrush;
+            }
+            else
+            {
+                return Brushes.Transparent;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+
+    public class DotBorderConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isSelected = value is true;
+            return isSelected ? new Thickness(0) : new Thickness(2);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
